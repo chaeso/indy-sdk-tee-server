e1d043c34f4f0b0b0595f8f8fddb89dbfad84867
hcchae <hc.chae@sk.com>
2020-06-23 17:12:00 +0900

    use SoftHSM encryption/decryption

--- src/main/java/com/sk/indysdkdemo/CryptoExample.java
+++ src/main/java/com/sk/indysdkdemo/CryptoExample.java
@@ -2,8 +2,38 @@ package com.sk.indysdkdemo;
 
 import com.sun.jna.Callback;
 import com.sun.jna.ptr.IntByReference;
+import org.hyperledger.indy.sdk.crypto.Crypto;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+import java.io.IOException;
 
 public class CryptoExample {
+
+    private class ReqResult {
+        String result;
+        String status;
+    }
+
+    private interface EncryptService {
+        @FormUrlEncoded
+        @POST("action/SymmetricEncrypt")
+        Call<ReqResult> encrypt(@Field("label") String label,
+                                  @Field("class") String cls,
+                                  @Field("msg") String msg);
+    }
+
+    private interface DecryptService {
+        @FormUrlEncoded
+        @POST("action/SymmetricDecrypt")
+        Call<ReqResult> encrypt(@Field("label") String label,
+                                @Field("class") String cls,
+                                @Field("msg") String msg);
+    }
 
     public static Callback keyGenCb = new Callback() {
 
@@ -15,10 +45,15 @@ public class CryptoExample {
     public static Callback encryptCb = new Callback() {
 
         @SuppressWarnings({ "unused", "unchecked" })
-        public String callback(int xcommand_handle, String msg, int l, IntByReference resultLen) {
-            String result = msg;
+        public String callback(int xcommand_handle, String msg, int l, IntByReference resultLen) throws IOException {
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl("http://127.0.0.1:7999/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+            EncryptService service = retrofit.create(EncryptService.class);
 
-            // Insert TEE decryption here
+            String result = service.encrypt("PAULAES", "CKO_SECRET_KEY", msg).execute().body().result;
+            System.out.println("result = " + result);
             resultLen.setValue(result.length());
             return result;
         }
@@ -27,10 +62,15 @@ public class CryptoExample {
     public static Callback decryptCb = new Callback() {
 
         @SuppressWarnings({ "unused", "unchecked" })
-        public String callback(int xcommand_handle, String msg, int l, IntByReference resultLen) {
-            String result = msg;
+        public String callback(int xcommand_handle, String msg, int l, IntByReference resultLen) throws IOException {
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl("http://127.0.0.1:7999/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+            DecryptService service = retrofit.create(DecryptService.class);
 
-            // Insert TEE encryption here
+            String result = service.encrypt("PAULAES", "CKO_SECRET_KEY", msg).execute().body().result;
+            System.out.println("result = " + result);
             resultLen.setValue(result.length());
             return result;
         }
